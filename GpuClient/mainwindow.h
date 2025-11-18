da6a78a117b1d6406ccfe0aca2c6bf1c37c57dfc
許芸綺 <b1243012@ems.niu.edu.tw>
2025-11-18 21:35:58 +0800

    Add files via upload
    先上傳最初始版本，還沒做修改

---
+++ GpuClient/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void onConnected();
+    void onReadyRead();
+    void onErrorOccurred(QAbstractSocket::SocketError socketError);
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket *socket;
+    QByteArray buffer;   //暫存尚未分行的資料
+};
+
+#endif // MAINWINDOW_H
