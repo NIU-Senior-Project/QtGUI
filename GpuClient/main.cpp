bba2b6682f9ccee73bf83efd7ebf4ea369c1a321
dot0427 <victoriachen305@gmail.com>
2026-02-27 18:15:05 +0800

    debug github保留問題

--- GpuClient/main.cpp
+++ GpuClient/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -22,28 +21,3 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-
-#include <QApplication>
-#include <QLocale>
-#include <QTranslator>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "GpuClient_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
->>>>>>> da6a78a117b1d6406ccfe0aca2c6bf1c37c57dfc
